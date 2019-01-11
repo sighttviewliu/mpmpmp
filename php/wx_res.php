6fe894fef46c79a1ff04487d68a3b9aa5f85d7b0
michael <denchance@gmail.com>
2019-01-11 10:52:42 +0800

    title: self define wechat response comment: ...

--- php/wx_res.php
+++ php/wx_res.php
@@ -1,11 +1,14 @@
 <?php  
     define("TOKEN", "michaellaoliu"); //TOKEN值  
     $wechatObj = new wechat();  
-    $wechatObj->valid();  
-    
+    ##$wechatObj->valid();  
+    $wechatObj->responseMSG();
+
     class wechat {  
+
         public function valid() {  
             $echoStr = $_GET["echostr"];  
+
             if($this->checkSignature()){  
                 echo $echoStr;  
                 exit;  
@@ -13,6 +16,7 @@
         }  
       
         private function checkSignature() {  
+
             $signature = $_GET["signature"];  
             $timestamp = $_GET["timestamp"];  
             $nonce = $_GET["nonce"];  
@@ -27,5 +31,70 @@
                 return false;  
             }  
         }  
+
+        public function responseMSG() {
+
+            $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+            if (!empty($postStr)) {
+
+                libxml_disable_entity_loader(true);
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $msgType = $postObj->MsgType;
+                $event = $postObj->Event;
+                $keyword = trim($postObj->Content);
+                $time = time();
+
+                $textTpl = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                <FuncFlag>0</FuncFlag>
+                            </xml>";
+
+                switch ($msgType)
+                {
+                    case "event":
+                    if ($event == "subscribe")
+                    {
+                        $contentStr = "------ \n press 1 \n press 2 \n press 3";
+                    }
+                    break;
+
+                    case "text":
+                    switch ($keyword)
+                    {
+                        case "1":
+                        $contentStr = "123";
+                        break;
+
+                        case "2":
+                        $contentStr = "ABC";
+                        break;
+
+                        case "3":
+                        $contentStr = "xyz";
+                        break;
+
+                        default:
+                        $contentStr = "nothing found!";
+                        break;
+                    }
+                    break;
+                }
+
+                $msgType = "text";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+            else {
+                echo "";
+                exit;
+            }
+        }
     }  
 ?>  
